164b625ae08f716b8deb51e5e6513cae5bb93bb0
Subhansh <subhansh.sharma@netcracker.com>
2021-01-24 15:08:49 +0530

    Constructor and Class Learning

---
+++ src/GetterSetterMethod.java
@@ -0,0 +1,9 @@
+public class GetterSetterMethod {
+    public static void main(String[] args){
+        Automobile A1= new Automobile("Ford","Ecosport","2013");
+        System.out.println(A1.make);
+        System.out.println(A1.model);
+        System.out.println(A1.year);
+    }
+
+}
